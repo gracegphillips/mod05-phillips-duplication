42c55df33cd9ae484099b2242b343d4ec4234afe
Grace <grace.phillips@bobcats.gcsu.edu>
2024-10-07 11:32:01 -0400

    Second time

--- app/routes.py
+++ app/routes.py
@@ -2,10 +2,15 @@ from flask import render_template
 from . import app
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/animals')
+def animals():
+    return render_template('animals.html')
+
 
